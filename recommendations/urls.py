720f33adb015b2fb2af95516977c7928647cd548
berk <berkoztug@hotmail.com>
2025-05-14 00:37:38 +0300

    urls.py healthz 4

--- recommendations/urls.py
+++ recommendations/urls.py
@@ -1,11 +1,11 @@
 # recommendations/urls.py
 from django.urls import path
-from .views import trending_products, health_check
+from .views import trending_products
 
 urlpatterns = [
     #path('', get_recommendations, name='get_recommendations'),
    # path('import-ga4/', import_from_ga4, name='import_from_ga4'),
     path('trending/', trending_products, name='trending_products'),
-    path('', health_check, name='health_check'),
+
 
 ]
