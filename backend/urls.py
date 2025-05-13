2880ff1b18326aa88fc4a94d4afec6a8c6c09eb1
berk <berkoztug@hotmail.com>
2025-05-14 00:30:11 +0300

    urls.py healthz 2

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 
-from backend.recommendations.views import health_check
+
 
 urlpatterns = [
     path('', lambda request: HttpResponse("Welcome to SearchBox API")),  # geçici test
@@ -10,5 +10,5 @@ urlpatterns = [
     path('api/product/', include('products.urls')),
     path('api/', include('recommendations.urls')),
     path("api/ga4/", include("analytics.urls")),
-    path("healthz", health_check, name="health_check"),
+    path("healthz", include("recommendations.urls")),
 ]
