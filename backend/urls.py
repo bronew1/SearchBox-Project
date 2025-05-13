e06f133ff01ff89e24c57480c3a53e1b32b6776e
berk <berkoztug@hotmail.com>
2025-05-13 15:35:14 +0300

    feat: added root path response

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Welcome to SearchBox API")),  # geçici test
     path('admin/', admin.site.urls),
     path('api/product/', include('products.urls')),
     path('api/', include('recommendations.urls')),
