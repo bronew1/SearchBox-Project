4d0bb1b97475509f2ae88435f5610b344dcbc3cc
bronew1 <berkoztug@hotmail.com>
2025-05-20 16:10:16 +0300

    tracking model push

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -1,9 +1,4 @@
 # recommendations/admin.py
 from django.contrib import admin
-from .models import UserInteraction
 
-@admin.register(UserInteraction)
-class UserInteractionAdmin(admin.ModelAdmin):
-    list_display = ("user_id", "product", "event_type", "timestamp")
-    list_filter = ("event_type", "timestamp")
-    search_fields = ("user_id", "product__title")
+
--- tracking/models.py
+++ tracking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from products.models import Product  # Ürünün kaynağı
 
-# Create your models here.
+class UserInteraction(models.Model):
+    user_id = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    event_type = models.CharField(max_length=50)  # örn: 'add_to_cart', 'purchase', 'view'
+    timestamp = models.DateTimeField()
