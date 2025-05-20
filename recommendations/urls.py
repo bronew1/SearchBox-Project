750a6d66a164e88ebc4fbb8204f89dcefcf48119
bronew1 <berkoztug@hotmail.com>
2025-05-21 02:35:08 +0300

    recommandetation push

--- recommendations/urls.py
+++ recommendations/urls.py
@@ -1,11 +1,12 @@
 # recommendations/urls.py
 from django.urls import path
-from .views import trending_products
+from .views import get_recommendations, trending_products
 
 urlpatterns = [
     #path('', get_recommendations, name='get_recommendations'),
    # path('import-ga4/', import_from_ga4, name='import_from_ga4'),
     path('trending/', trending_products, name='trending_products'),
+    path("", get_recommendations, name="get_recommendations")
 
 
 ]
--- recommendations/views.py
+++ recommendations/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from .services.ga4_fetcher import get_top_products
+from tracking.models import UserEvent
+from products.models import Product  # senin ürün modelin buysa
+from django.views.decorators.csrf import csrf_exempt
 
 def trending_products(request):
     event_type = request.GET.get("type", "purchase")  # "purchase" veya "view_item"
@@ -11,3 +14,30 @@ def trending_products(request):
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
 
+
+@csrf_exempt
+def get_recommendations(request):
+    user_id = request.GET.get("user_id")
+
+    if not user_id:
+        return JsonResponse({"error": "user_id missing"}, status=400)
+
+    # Bu kullanıcı geçmişte neleri görüntülemiş?
+    events = UserEvent.objects.filter(user_id=user_id, event_name="view_item").order_by("-timestamp")
+
+    # En son baktığı ürünlerden en fazla 10 tanesini çek
+    product_ids = list(events.values_list("product_id", flat=True).distinct()[:10])
+
+    # Ürünleri ürün tablosundan getir
+    recommended_products = Product.objects.filter(sku__in=product_ids)
+
+    result = []
+    for product in recommended_products:
+        result.append({
+            "name": product.name,
+            "price": product.price,
+            "image": product.image_url,
+            "sku": product.sku,
+        })
+
+    return JsonResponse({"recommendations": result})
