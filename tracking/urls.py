5c23e4fdb5b69c6ec0e059b578955b9d0b3aa75b
bronew1 <berkoztug@hotmail.com>
2025-05-20 16:06:48 +0300

    tracking push

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,6 +1,7 @@
+# tracking/urls.py
 from django.urls import path
-from tracking.views import track_event
+from .views import track_event
 
 urlpatterns = [
-    path('track-event/', track_event, name='track-event')
+    path("track-event/", track_event),
 ]
