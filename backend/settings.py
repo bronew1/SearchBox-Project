adc6d55758c33547caca01c6db8ed523ea46ac21
bronew1 <berkoztug@hotmail.com>
2025-05-25 00:17:30 +0300

    database revize .env

--- backend/settings.py
+++ backend/settings.py
@@ -99,11 +99,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 # Password validation
