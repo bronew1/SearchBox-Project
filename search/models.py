f5e352bbf0aaaa4563a57b3ee2e9c000d84ec8b3
bronew1 <berkoztug@hotmail.com>
2025-05-25 19:54:17 +0300

    60 ürün

--- search/models.py
+++ search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class ProductEmbedding(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    embedding = models.JSONField()  # Embedding vektörü burada tutulacak
+
+    def __str__(self):
+        return f"Embedding for {self.product.sku}"
