901341a2972864e331e855a161f13fd6d446f71b
bronew1 <berkoztug@hotmail.com>
2025-06-01 21:19:13 +0300

    sina yeni mail

--- backend/settings.py
+++ backend/settings.py
@@ -178,10 +178,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = "8e19eb003@smtp-brevo.com"  # Brevo SMTP Login
+EMAIL_HOST_USER = "8e19eb003@smtp-brevo.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "no-reply@searchprojectdemo.com"  # Doğrulanmış gönderen
+DEFAULT_FROM_EMAIL = "8e19eb003@smtp-brevo.com"
+
 
 
 LOGGING = {
--- subscriptions/utils.py
+++ subscriptions/utils.py
@@ -1,24 +1,24 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.conf import settings
 import logging
+
 logger = logging.getLogger('subscriptions')
 
 def send_welcome_email(email):
     logger.info(f"📬 Mail fonksiyonu tetiklendi: {email}")
-    subject = "Aramıza Hoş Geldin!"
-    from django.template.loader import render_to_string
-    from django.core.mail import EmailMultiAlternatives
 
-    html_content = render_to_string("email/welcome.html", {"email": email})
+    subject = "Aramıza Hoş Geldin!"
     text_content = "Merhaba, aramıza hoş geldiniz!"
+    html_content = render_to_string("email/welcome.html", {"email": email})
 
     logger.info("✅ HTML içerik oluşturuldu")
 
     msg = EmailMultiAlternatives(
-        subject,
-        text_content,
-        "SearchProjectDemo <no-reply@searchprojectdemo.com>",
-        [email]
+        subject=subject,
+        body=text_content,
+        from_email=settings.DEFAULT_FROM_EMAIL,  # ✔️ Ayarı settings.py'den al
+        to=[email]
     )
     msg.attach_alternative(html_content, "text/html")
     msg.send()
