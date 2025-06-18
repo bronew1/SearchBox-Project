6d04b1e4f9074a71e7defd2c40a4769e772f06f6
bronew1 <berkoztug@hotmail.com>
2025-06-18 16:20:53 +0300

    static dosya test

--- static/widgets/widget.js
+++ static/widgets/widget.js
@@ -105,7 +105,7 @@
 
     const toggle = document.createElement("div");
     toggle.id = "best-seller-toggle";
-    toggle.innerText = "🛍️";
+    toggle.innerText = "Günün💎Fırsatı";
     toggle.onclick = () => {
       state.visible = !state.visible;
       document.getElementById("best-seller-popup").style.display = state.visible ? "block" : "none";
