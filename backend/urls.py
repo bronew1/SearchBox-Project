ba649b0147f182e9c5ba7c3d0d91437d713b90ca
bronew1 <berkoztug@hotmail.com>
2025-06-18 13:34:07 +0300

    urls.py hata düzeltme

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("api/track-event/", track_event, name="track_event"),
     path('api/subscribe/', subscribe),
     path("api/cart-count/<str:product_id>/", cart_count),
-    path("api/", include("products.urls")),
+   # path("api/", include("products.urls")),
     
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
