ef1f6f6c1683b4e0e8073b3a2d65e0c93ef00f0d
bronew1 <berkoztug@hotmail.com>
2025-06-21 14:31:25 +0300

    products app güncelleme

--- products/utils/xml_parser.py
+++ products/utils/xml_parser.py
@@ -1,20 +1,21 @@
 import xml.etree.ElementTree as ET
 
 def parse_xml_and_extract_products(xml_file):
+    import xml.etree.ElementTree as ET
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
     products = []
-    for item in root.findall("item"):
+    for item in root.findall("product"):
         product = {
-            "external_id": item.findtext("g:id", default=""),
-            "sku": item.findtext("g:mpn", default=""),
-            "name": item.findtext("title", default=""),
+            "external_id": item.findtext("id", default=""),
+            "sku": item.findtext("sku") or item.findtext("mpn") or item.findtext("id"),
+            "title": item.findtext("name"),
             "description": item.findtext("description", default=""),
-            "price": item.findtext("g:price", default="").replace(" TRY", ""),
-            "image_url": item.findtext("g:image_link", default=""),
-            "product_url": item.findtext("link", default=""),
-            "category": item.findtext("g:product_type", default=""),
+            "price": item.findtext("price"),
+            "image_url": item.findtext("image"),
+            "category": item.findtext("category", default=""),
+            "product_url": item.findtext("product_url") or item.findtext("link") or "",  # 🟢 buraya dikkat
         }
         products.append(product)
     return products
