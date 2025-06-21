7cc05bc81284dbeba3b2c9b3905a4b6a0b1f5361
bronew1 <berkoztug@hotmail.com>
2025-06-21 14:10:09 +0300

    product xml yükleme güncelleme

--- products/utils/xml_parser.py
+++ products/utils/xml_parser.py
@@ -3,22 +3,18 @@ import xml.etree.ElementTree as ET
 def parse_xml_and_extract_products(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    ns = {'g': 'http://base.google.com/ns/1.0'}
 
     products = []
-
-    for item in root.findall("channel/item"):
+    for item in root.findall("item"):
         product = {
-            "external_id": item.findtext("g:id", default="", namespaces=ns),
-            "sku": item.findtext("g:mpn", default="", namespaces=ns),
-            "name": item.findtext("title", default=""),
+            "external_id": item.findtext("g:id", default=""),
+            "sku": item.findtext("g:mpn", default=""),
+            "title": item.findtext("title"),
             "description": item.findtext("description", default=""),
-            "price": item.findtext("g:price", default="", namespaces=ns).replace("TRY", "").strip(),
-            "image_url": item.findtext("g:image_link", default="", namespaces=ns),
-            "product_url": item.findtext("link", default=""),
-            "category": item.findtext("g:product_type", default="", namespaces=ns),
+            "price": item.findtext("g:price", default="0").replace(" TRY", "").replace(",", ""),
+            "image_url": item.findtext("g:image_link", default=""),
+            "category": item.findtext("g:product_type", default=""),
+            "product_url": item.findtext("link", default="")  # 🔥 en önemlisi bu
         }
         products.append(product)
-
     return products
-
