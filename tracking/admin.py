35fd7b464431de722b9af6120dedc1b896a19180
bronew1 <berkoztug@hotmail.com>
2025-06-27 17:19:20 +0300

    test

--- tracking/admin.py
+++ tracking/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import CartAbandonment, UserEvent
+from .models import CartAbandonment, PushSubscription, UserEvent
 
 @admin.register(UserEvent)
 class UserEventAdmin(admin.ModelAdmin):
@@ -14,3 +14,7 @@ class CartAbandonmentAdmin(admin.ModelAdmin):
     list_display = ["user_id", "product_id", "added_at", "is_purchased", "is_email_sent"]  # ✅ DOĞRU HALİ
     list_filter = ["is_email_sent", "is_purchased"]  # ✅ DOĞRU HALİ
 
+@admin.register(PushSubscription)
+class PushSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('endpoint', 'user_id', 'created_at')
+    search_fields = ('user_id', 'endpoint')
