cac090358a489418936577acccc6a04fb9c571ad
bronew1 <berkoztug@hotmail.com>
2025-06-27 12:05:30 +0300

    urls ana proje güncelleme

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
-from tracking.views import cart_count, track_event
+from tracking.models import save_subscription
+from tracking.views import cart_count, public_vapid_key, track_event
 from subscriptions.views import subscribe 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -20,6 +21,9 @@ urlpatterns = [
     path("api/track-event/", track_event, name="track_event"),
     path('api/subscribe/', subscribe),
     path("api/cart-count/<str:product_id>/", cart_count),
+    path("api/public-key/", public_vapid_key),
+    path("api/save-subscription/", save_subscription),
+    
    # path("api/", include("products.urls")),
     
     
