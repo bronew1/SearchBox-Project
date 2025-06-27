84508ab8bd88943789d3300ef62d3ecc9194ec13
bronew1 <berkoztug@hotmail.com>
2025-06-27 12:04:47 +0300

    static güncelleme web push

---
+++ static/service-worker.js
@@ -0,0 +1,15 @@
+// static/service-worker.js
+
+self.addEventListener('push', function(event) {
+  const data = event.data.text();
+
+  const options = {
+    body: data,
+    icon: '/favicon.ico',
+    badge: '/favicon.ico'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification("Sina Pırlanta", options)
+  );
+});
