a2b66d6cd910f80a1c96760e592da71fbdf2be0b
bronew1 <berkoztug@hotmail.com>
2025-07-02 16:29:22 +0300

    url güncelleme

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,8 +1,9 @@
 # subscriptions/urls.py
 from django.urls import path
-from .views import get_welcome_email_template, subscribe
+from .views import get_welcome_email_template, subscribe, update_welcome_email_template
 
 urlpatterns = [
     path("", subscribe),
     path("welcome-template/", get_welcome_email_template),
+     path("welcome-template/update/", update_welcome_email_template),
 ]
