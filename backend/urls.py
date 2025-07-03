7cc9025d713dc038bd46fc1223924b24a27979cb
bronew1 <berkoztug@hotmail.com>
2025-07-03 16:51:13 +0300

    url analytics ana proje güncelleme

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/product/', include('products.urls')),
     path('api/recommendations/', include('recommendations.urls')),
-    path("api/ga4", include("analytics.urls")),
+    path("api/ga4/", include("analytics.urls")),
     path("healthz", health_check),
     path("api/track-event/", track_event, name="track_event"),
     path('api/subscribe/', include('subscriptions.urls')),
