a3b47408fc4d55c34f2089a413804161b099662a
bronew1 <berkoztug@hotmail.com>
2025-07-03 17:11:47 +0300

    güncelleme

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import revenue_view, search_terms_view
 
+
 urlpatterns = [
     path("search-terms/", search_terms_view, name="search-terms"),
     path("revenue/", revenue_view, name="revenue"),
-    
 ]
