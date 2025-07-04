608405b89135ef3bc25da6df9d0639085286a1bc
bronew1 <berkoztug@hotmail.com>
2025-07-04 17:54:23 +0300

    campanins güncelleme

--- campains/urls.py
+++ campains/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import create_campaign, list_campaigns
 
 urlpatterns = [
-    path("create/", create_campaign, name="create_campaign"),
-    path("list/", list_campaigns, name="list_campaigns"),
+     path("", list_campaigns, name="list_campaigns"),  # Listeleme
+    path("create/", create_campaign, name="create_campaign"),  
 ]
