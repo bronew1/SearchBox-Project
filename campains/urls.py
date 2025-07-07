56099381183c942d2d68a19b301dfc5861e5083f
bronew1 <berkoztug@hotmail.com>
2025-07-07 11:03:20 +0300

    campains get güncelleme

--- campains/urls.py
+++ campains/urls.py
@@ -4,4 +4,5 @@ from .views import create_campaign, list_campaigns
 urlpatterns = [
     path("", list_campaigns, name="list_campaigns"), 
     path("create/", create_campaign, name="create_campaign"),  
+    path("<int:pk>/", campaign_detail, name="campaign_detail"),
 ]
