e477ac76297cd87fd41de7d1e0d03b1edd12e4a3
bronew1 <berkoztug@hotmail.com>
2025-07-07 12:36:51 +0300

    kampanya mail güncelleme

--- campains/urls.py
+++ campains/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import campaign_detail, create_campaign, list_campaigns
+from .views import campaign_detail, create_campaign, delete_campaign, list_campaigns
 
 urlpatterns = [
     path("", list_campaigns, name="list_campaigns"), 
     path("create/", create_campaign, name="create_campaign"),  
     path("<int:pk>/", campaign_detail, name="campaign_detail"),
-    path("delete/<int:pk>/", views.delete_campaign, name="delete_campaign"),
+    path("delete/<int:pk>/", delete_campaign, name="delete_campaign"),
 ]
+
+#
