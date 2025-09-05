8d30bec1b8ec4413187ad50c7e8c361eba0f38db
bronew1 <berkoztug@hotmail.com>
2025-09-05 09:57:47 +0300

    config güncelleme

--- sinafrontend/next.config.ts
+++ sinafrontend/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",   // ✅ static export aktif
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  // Eğer auth gibi dinamik route'ların static export ile problemi olmasın istiyorsan
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
