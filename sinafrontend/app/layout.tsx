c6d12fab1407027adad0625c403c052ec6d5787a
bronew1 <berkoztug@hotmail.com>
2025-09-05 17:03:38 +0300

    vercel.svg push

--- sinafrontend/app/layout.tsx
+++ sinafrontend/app/layout.tsx
@@ -1,5 +1,13 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Sina Pırlanta CXP",
+  description: "Müşteri Deneyim Platformu",
+  icons: {
+    icon: "/vercel.svg", // public/vercel.svg
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="tr">
