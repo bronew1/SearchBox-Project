b81dd8079af0ced5e7f7a65fc0308d59571559e4
bronew1 <berkoztug@hotmail.com>
2025-09-05 09:44:29 +0300

    config güncelleme

--- sinafrontend/next.config.ts
+++ sinafrontend/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",   // ✅ static export aktif
-  images: {
-    unoptimized: true // ✅ Render'da image optimizer çalışmaz, bu ayarı ekle
-  }
+  
 };
 
 export default nextConfig;
