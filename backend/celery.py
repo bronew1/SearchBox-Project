531d3a470aa010ea060420ecc2dd8f55545e6012
bronew1 <berkoztug@hotmail.com>
2025-09-08 21:33:48 +0300

    celery güncelleme 2

--- backend/celery.py
+++ backend/celery.py
@@ -4,14 +4,10 @@ from celery import Celery
 
 app = Celery("backend")
 
-# Ortak ayarlar
 app.conf.broker_url = os.getenv("CELERY_BROKER_URL")
 app.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND")
 
-# SSL ayarları (rediss:// için gerekli)
-app.conf.broker_use_ssl = {
-    "ssl_cert_reqs": ssl.CERT_NONE
-}
-app.conf.redis_backend_use_ssl = {
-    "ssl_cert_reqs": ssl.CERT_NONE
-}
+app.conf.broker_use_ssl = {"ssl_cert_reqs": ssl.CERT_NONE}
+app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": ssl.CERT_NONE}
+
+app.autodiscover_tasks()
