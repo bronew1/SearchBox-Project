4f83bec6f495f6635ab75238abc480355253ee69
bronew1 <berkoztug@hotmail.com>
2025-09-08 17:39:02 +0300

    init güncellememee

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,3 @@
-try:
-    from .celery import app as celery_app
-except ImportError:
-    # Render gibi src yapısında sorun olursa fallback
-    import importlib
-    celery_app = importlib.import_module("backend.celery").app
+from .celery import app as celery_app
 
 __all__ = ("celery_app",)
