b89a7fb8936b077d682de450adfc19a2fadfb111
bronew1 <berkoztug@hotmail.com>
2025-09-08 17:05:17 +0300

    init güncelleme

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     # Render gibi src yapısında sorun olursa fallback
     import importlib
-    celery_app = importlib.import_module("src.backend.celery").app
+    celery_app = importlib.import_module("backend.celery").app
 
 __all__ = ("celery_app",)
